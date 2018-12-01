a50b967ec740bae6bd2653c233a6d3a21746120b
Karina Vicente <karinavicente@karinas-macbook.local>
2018-12-01 00:54:16 -0800

    Initial Commit

---
+++ Parse Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parse Chat
+//
+//  Created by Karina Vicente on 12/1/18.
+//  Copyright © 2018 Karina Vicente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
